10e6802b947846cd8eb42df90cf0bfd6ae4215cb
gengmzh <gengmzh@gmail.com>
2012-05-09 17:23:56 +0800

    add mongo-hadoop

---
+++ _includes/MongoInputFormat.mm
@@ -0,0 +1,16 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1336552951401" ID="ID_699756764" MODIFIED="1336554374085" TEXT="MongoInputFormat">
+<node CREATED="1336553095815" ID="ID_1996454763" MODIFIED="1336553104626" POSITION="right" TEXT="getSplits">
+<node CREATED="1336553179853" ID="ID_902133128" MODIFIED="1336553211434" TEXT="&#x6839;&#x636e;Job Config&#x6784;&#x9020;MongoConfig"/>
+<node CREATED="1336553231964" ID="ID_1308217215" MODIFIED="1336553488554" TEXT="&#x4f7f;&#x7528;MongoSplitter&#x8ba1;&#x7b97;InputSplit">
+<node CREATED="1336553360531" ID="ID_507058618" MODIFIED="1336553589135" TEXT="&#x4e0d;&#x521b;&#x5efa;InputSplit&#x65f6;&#xff0c;&#x5c06;&#x8fd9;&#x4e2a;&#x8f93;&#x5165;&#x4f5c;&#x4e3a;&#x4e00;&#x4e2a;InputSplit&#x8fd4;&#x56de;"/>
+<node CREATED="1336553592777" ID="ID_990553979" MODIFIED="1336554280545" TEXT="&#x5206;&#x7247;&#x67b6;&#x6784;">
+<node CREATED="1336553689366" ID="ID_1156144291" MODIFIED="1336554370229" TEXT="&#x901a;&#x8fc7;chunk&#x521b;&#x5efa;InputSplit&#x8fd4;&#x56de;"/>
+<node CREATED="1336553706790" ID="ID_167359782" MODIFIED="1336554372725" TEXT="&#x901a;&#x8fc7;shard&#x521b;&#x5efa;InputSplit&#x8fd4;&#x56de;"/>
+</node>
+<node CREATED="1336553723550" ID="ID_914724105" MODIFIED="1336554309326" TEXT="&#x5c06;&#x96c6;&#x5408;&#x6309;&#x6307;&#x5b9a;&#x5757;&#x5927;&#x5c0f;&#x5206;&#x5272;&#x4e3a;&#x591a;&#x4e2a;InputSplit"/>
+</node>
+</node>
+</node>
+</map>
